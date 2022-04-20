d13bb50d34eb4fadbb6368f7b9260c18e511fe3f
bjih1999 <bjih1999@naver.com>
2022-04-20 22:53:35 +0900

    feat: Exception filer 공부한 것 정리

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Delete, Get, Patch, Post, Put } from "@nestjs/common";
+import { Controller, Delete, Get, HttpException, HttpStatus, Patch, Post, Put, UseFilters } from "@nestjs/common";
 import { CatsService } from './cats.service';
+import { HttpExceptionFilter } from "../http-exception.filter";
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
+  @UseFilters(HttpExceptionFilter)  // 필터를 적용할 대상위에 달아주면 예외 처리 필터가 적용됨
   getAllCat() {
+    /*
+    - 단순 Error로 통일된 express와 달리 nest에는 HttpExeption이 존재함
+    - HttpException이기 때문에 네트워크 레이어와 근접한 Controller 단에서 발생시키는 것이 바람직해 보인다.
+     */
+    throw new HttpException(
+      '인증되지 않은 회원입니다.',
+      HttpStatus.UNAUTHORIZED,
+    );
     return 'all cat';
   }
 
