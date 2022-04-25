dbc1c801f2da68302f08d062165d0a9bb15cfd2b
Jihyun <bjih1999@naver.com>
2022-04-25 15:33:39 +0900

    feat: dto validation pipe 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from "./common/exceptions/http-exception.filter";
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // dto validtion 적용
+  app.useGlobalPipes(new ValidationPipe());
   // 전역에 필터 적용
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT);
